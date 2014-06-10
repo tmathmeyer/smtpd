177dcc906146b7a6ba55de1e214f7c66fedb9601
Ted Meyer <tmathmeyer@gmail.com>
2014-06-10 14:31:48 -0400

    add the code for detecting circles

--- src/smtpd.c
+++ src/smtpd.c
@@ -1,23 +1,10 @@
 
 #include "parse.h"
+#include "smtpd.h"
 #include "action_identify.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
-
-#define MAX(A,B) (A>B?A:B)
-#define ABS(A) (A>0?A:-A)
-
-typedef struct sample {
-    struct sample* next;
-    status* data;
-} sample;
-
-int analyze(sample* motionlist);
-int daemon_function();
+#include <math.h>
 
 
 
@@ -30,30 +17,57 @@ int main(int argc, char** argv) {
 
 }
 
+float dist(status* a, status* b) {
+    int x = a -> contact_xpos - b -> contact_xpos;
+    int y = a -> contact_ypos - b -> contact_ypos;
+    return sqrt(x*x + y*y);
+}
+
+float calc_angle(status* a, status* b, status* c) {
+    float A = dist(a, b);
+    float B = dist(a, c);
+    float C = dist(b, c);
+    float arg = (C*C - A*A - B*B) / (-2*A*B);
+    return acos(arg);
+}
+
+float circle_analyzer(sample* motionlist) {
+    float angle = 0.0;
+    status *a = NULL, *b = NULL, *c = NULL;
+    while(motionlist -> next != NULL) {
+        a = b; b = c;
+        c = motionlist -> data;
+        if (a != NULL  &&  b != NULL  &&  c != NULL) {
+            angle += calc_angle(a, b, c);
+        }
+        motionlist = motionlist -> next;
+    }
+    return angle;
+}
 
 
 
 
-
-
-// do this neively for now
+// do this neively for now (dont even bother freeing the list)
 int analyze(sample* motionlist) {
     if (motionlist == NULL) {
         return -1;
     }
 
-    status* initial = motionlist -> data;
-    int max_fingers = 0;
-    while(motionlist -> next != NULL) {
-        max_fingers = MAX(max_fingers, motionlist -> data -> contact_points);
-        motionlist = motionlist -> next;
-    }
-    status* final = motionlist -> data;
+    //status* initial = motionlist -> data;
+    //int max_fingers = 0;
+    //while(motionlist -> next != NULL) {
+    //    max_fingers = MAX(max_fingers, motionlist -> data -> contact_points);
+    //    motionlist = motionlist -> next;
+    //}
+    //status* final = motionlist -> data;
 
-    system(identify_action(final->reading_time-initial->reading_time,
-                           final->contact_xpos-initial->contact_xpos,
-                           final->contact_ypos-initial->contact_ypos,
-                           max_fingers));
+    //puts(identify_action(final->reading_time-initial->reading_time,
+    //                       final->contact_xpos-initial->contact_xpos,
+    //                       final->contact_ypos-initial->contact_ypos,
+    //                       max_fingers));
+
+    printf("%f\n", circle_analyzer(motionlist));
     return 0;
 }
 
